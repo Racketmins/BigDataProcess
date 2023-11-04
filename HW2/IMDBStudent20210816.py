136530b6042071363dc91c2880cefcbf0c643e4e
Racketmins <113955657+Racketmins@users.noreply.github.com>
2023-11-04 22:33:07 +0900

    Add files via upload

--- HW2/IMDBStudent20210816.py
+++ HW2/IMDBStudent20210816.py
@@ -1,1 +1,33 @@
+def counter(filename, m):
+	try:
+		f = open(filename)
+		for line in f:
+			info = line.strip().split("::")
+			genre = info[2].split('|')
+			for i in genre:
+				if i not in m:
+					m[i] = 1
+				else:
+					m[i] += 1
+		return m
+	except FileNotFoundError:
+		print("Not file from counter")
+	finally:
+		f.close()
 
+def saveList(filename, m):
+	try:
+		f = open(filename, "wt")
+		keyList = m.keys()
+		for i in keyList:
+			f.write(str(i) + " "  + str(m[i]) + "\n")
+	except FileNotFoundError:
+		print("Not file from save")
+	finally:
+		f.close()
+
+movies = dict()
+inputFile  = "movie.dat"
+outputFile = "movieoutput.txt"
+movies = counter(inputFile, movies)
+saveList(outputFile, movies)
