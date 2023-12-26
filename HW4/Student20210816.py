574da520cfc05cbc46e9e6d46906ea2c96b69fc0
Racketmins <113955657+Racketmins@users.noreply.github.com>
2023-12-27 00:31:40 +0900

    Update Student20210816.py

--- HW4/Student20210816.py
+++ HW4/Student20210816.py
@@ -1,30 +1,29 @@
 import sys
 import os
 import numpy as np
-import operator
 from collections import Counter
 
 def load_data(folderName):
-	data = []
-	labels = []
-	for filename in os.listdir(folderName):
-        	if filename.endswith(".txt"):
-          	  	label = int(filename.split("_")[0])
-			filepath = os.path.join(folderName, filename)
-			with open(filepath, 'r') as file:
-                		content = file.read().replace('\n', '')
-             			data.append(list(map(int, content)))
-                		labels.append(label)
-	return np.array(data), np.array(labels)
+    data = []
+    labels = []
+    for filename in os.listdir(folderName):
+        if filename.endswith(".txt"):
+            label = int(filename.split("_")[0])
+            filepath = os.path.join(folderName, filename)
+            with open(filepath, 'r') as file:
+                content = file.read().replace('\n', '')
+                data.append(list(map(int, content)))
+                labels.append(label)
+    return np.array(data), np.array(labels)
 
-def calDistance(a, b):
-	return np.sqrt(np.sum((a - b)**2))
+def calculate_distance(a, b):
+    return np.sqrt(np.sum((a - b)**2))
 
-def classify0(inX, group, labels, k):
-	distances = [calDistance(inX, i) for i in group]
-	nearest = np.argsort(distances)[:k]
-	nearest_labels = labels[nearest]
-	return Counter(nearest_labels).most_common(1)[0][0]
+def classify0(input_data, training_data, training_labels, k):
+    distances = [calculate_distance(input_data, i) for i in training_data]
+    nearest_indices = np.argsort(distances)[:k]
+    nearest_labels = training_labels[nearest_indices]
+    return Counter(nearest_labels).most_common(1)[0][0]
 
 train_folder = sys.argv[1]
 test_folder = sys.argv[2]
@@ -33,10 +32,10 @@ train_data, train_labels = load_data(train_folder)
 test_data, test_labels = load_data(test_folder)
 
 for k in range(1, 21):
-	rate = 0
-	leng = len(test_data)
-	for i in range(leng):
-		answer = classify0(test_data[i], train_data, train_labels, k)
-		if answer != test_labels[i]:
-			rate += 1
-	print(int((rate / leng)*100))
+    error_count = 0
+    total_len = len(test_data)
+    for i in range(total_len):
+        prediction = classify0(test_data[i], train_data, train_labels, k)
+        if prediction != test_labels[i]:
+            error_count += 1
+    print(int((error_count / total_instances) * 100))
